8eef98db40d3c2fd93b4ab4765ac5fc68ee5e384
enderstream00 <enderstream00@naver.com>
2024-11-20 23:15:27 +0900

    부트스트랩 이슈 해결, 남은건 적용하기 뿐

--- dg-prj-front/src/main.js
+++ dg-prj-front/src/main.js
@@ -6,8 +6,8 @@ import App from './App.vue';
 import router from './router';
 // 부트스트랩
 import BootstrapVue3 from 'bootstrap-vue-3'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue3.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App);
 
